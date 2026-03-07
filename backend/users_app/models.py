11c4b00a9dfdc356b246c202702b26d5f9a708e5
TobiasKlanert <tobias.klanert@t-online.de>
2026-03-07 13:53:19 +0100

    fix: custom user model adapted so that admin users can be created

--- backend/users_app/models.py
+++ backend/users_app/models.py
@@ -15,6 +15,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     is_active = models.BooleanField(default=False)
     is_online = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
