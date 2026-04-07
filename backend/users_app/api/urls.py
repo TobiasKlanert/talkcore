bde8f7b410ad8326d3654d1cb7c3cc1aca5ea5a8
TobiasKlanert <social@tobias-klanert.de>
2026-04-07 21:44:09 +0200

    feat: search-user endpoint implemented

---
+++ backend/users_app/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import UserSearchView
+
+urlpatterns = [
+    path("users/", UserSearchView.as_view(), name="search-user")
+]
--- backend/users_app/api/views.py
+++ backend/users_app/api/views.py
@@ -1,3 +1,26 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from users_app.models import User
 
-# Create your views here.
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+
+class UserSearchView(ListAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = User.objects.all()
+
+        search = self.request.query_params.get("search")
+
+        if search:
+            queryset = queryset.filter(display_name__icontains=search)
+
+        return queryset.exclude(id=self.request.user.id)
