ef05674a4e8d4f3774fc58983b97349adb9d3754
TobiasKlanert <social@tobias-klanert.de>
2026-04-12 17:49:19 +0200

    feat: implement basic app shell with sidenav and topbar (wip)

--- frontend/app/src/app/core/layout/app-shell/app-shell.ts
+++ frontend/app/src/app/core/layout/app-shell/app-shell.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
-  selector: 'app-app-shell',
-  imports: [RouterOutlet, MatSidenav, MatSidenavContainer, MatSidenavContent],
+  selector: 'app-shell',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './app-shell.html',
   styleUrl: './app-shell.scss',
 })
-export class AppShell {}
+export class AppShell {
+  appName = 'TalkCore';
+}
