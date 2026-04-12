b1b8a3358342f208e3620d696764e64e39d0eaf7
TobiasKlanert <social@tobias-klanert.de>
2026-04-12 17:30:05 +0200

    feat: basic routing implemented

--- frontend/app/src/app/app.routes.ts
+++ frontend/app/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AppShell } from '@core/layout/app-shell/app-shell';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+  {
+    path: '',
+    // canActivate: [authGuard],
+    component: AppShell,
+    children: [
+      {
+        path: 'chat',
+        loadChildren: () => import('./features/chat/chat.routes').then((m) => m.CHAT_ROUTES),
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'chat',
+      },
+    ],
+  },
+];
